87273d48caf82d58ca0a6f00673f922aa0ba0ba7
Prajwal <prajwalhassan7@gmail.com>
2023-09-26 09:32:41 +0530

    Able to manage Members

--- components/providers/ModalProvider.tsx
+++ components/providers/ModalProvider.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { CreateServerModal } from "@/components/modals/CreateServerModal";
 import { InviteModal } from "../modals/InviteModal";
 import { EditServerModal } from "../modals/EditServerModal";
+import { MembersModal } from "../modals/MembersModal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -22,6 +23,7 @@ export const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal/>
       <EditServerModal/>
+      <MembersModal/>
     </>
   )
 }
